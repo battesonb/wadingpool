21483a48df05da427b0a741d91b49e5ec7f0beac
Byron Batteson <byronbatteson@gmail.com>
2024-04-28 13:11:38 +0100

    Add task support to ThreadPool
    The ThreadPool now handles tasks appropriately.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,41 @@
 use std::num::NonZeroUsize;
+use std::sync::{self, mpsc::Sender, Arc, Mutex};
 use std::thread::JoinHandle;
+
+type Task = Box<dyn FnOnce() -> () + Send>;
 
 pub struct ThreadPool {
     threads: Vec<JoinHandle<()>>,
+    sender: Sender<Task>,
 }
 
 impl ThreadPool {
     pub fn new(size: NonZeroUsize) -> Self {
         let size = size.get();
         let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = sync::mpsc::channel::<Task>();
 
+        let receiver = Arc::new(Mutex::new(receiver));
         for _ in 0..size {
-            let thread = std::thread::spawn(move || {
-                std::thread::park();
+            let receiver = receiver.clone();
+            let thread = std::thread::spawn(move || loop {
+                if let Ok(task) = {
+                    let rx = receiver.lock().unwrap();
+                    rx.recv()
+                } {
+                    task();
+                }
             });
             threads.push(thread);
         }
 
-        Self { threads }
+        Self { threads, sender }
+    }
+
+    pub fn spawn<F>(&mut self, callback: F)
+    where
+        F: FnOnce() -> () + Send + 'static,
+    {
+        self.sender.send(Box::new(callback)).unwrap();
     }
 }
